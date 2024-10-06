fd9f5d1011129d652eb54299f873b6b6327cb0b4
rahul <rahulshah@resimpli.com>
2024-10-07 01:29:48 +0530

    connectivity util added

--- app/src/main/java/com/example/myapplication/di/AppModule.kt
+++ app/src/main/java/com/example/myapplication/di/AppModule.kt
@@ -2,6 +2,7 @@ package com.example.myapplication.di
 
 import android.content.Context
 import com.example.myapplication.utils.AppPreferences
+import com.example.myapplication.utils.ConnectivityUtil
 import org.koin.dsl.module
 
 var appModule= module {
---
+++ app/src/main/java/com/example/myapplication/model/UserObject.kt
@@ -0,0 +1,5 @@
+package com.example.myapplication.model
+
+class UserObject(
+    val regNo: String?=null,
+)
